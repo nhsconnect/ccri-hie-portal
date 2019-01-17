3d82eb0a14f7f5157718192b9e935133d351d8a5
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2019-01-17 11:16:03 +0000

    Initial commit of hie portal app (moved from careconnect-reference-implementation)

--- hie-portal/src/app/app.component.spec.ts
+++ hie-portal/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'ccri-hie'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('ccri-hie');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hie-portal!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ccri-hie!');
   }));
 });
