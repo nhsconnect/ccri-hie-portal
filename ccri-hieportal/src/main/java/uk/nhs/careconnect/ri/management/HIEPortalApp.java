42d786026a9f25c5910ab792efdf3386be7b4db3
KevinMayfield <mayfield.g.kev@gmail.com>
2019-03-04 11:12:01 +0000

    Deploy checks

--- ccri-hieportal/src/main/java/uk/nhs/careconnect/ri/management/HIEPortalApp.java
+++ ccri-hieportal/src/main/java/uk/nhs/careconnect/ri/management/HIEPortalApp.java
@@ -40,7 +40,7 @@ public class HIEPortalApp {
             @Override
             public void beforeApplicationStart(CamelContext camelContext) {
 
-                camelContext.setNameStrategy(new DefaultCamelContextNameStrategy("CcriMonitor"));
+                camelContext.setNameStrategy(new DefaultCamelContextNameStrategy("CcHiePortal"));
 
             }
 
