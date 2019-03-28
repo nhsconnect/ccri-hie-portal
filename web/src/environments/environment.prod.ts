634567c3227c478d827f874d7a0c57eb4a328d33
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2019-03-28 11:13:05 +0000

    Spring Boot 2 mods

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
     oauth2 : {
-        eprUrl : 'http://127.0.0.1:8186/ccri-fhir/STU3',
+        eprUrl : 'https://data.developer.nhs.uk/ccri-fhir/STU3',
         client_id : 'nhs-smart-ehr',
         client_secret : 'APa5oCe6SHhty_or2q34WpNcq0-X957n6p48TkAJw14YCtmZeQil60XvCfuByIPd8DlXyusxAGxp5_Z5UKlgZJU',
-        cookie_domain : 'localhost',
-        logonUrl: 'https://data.developer-test.nhs.uk/document-viewer'
+        cookie_domain : 'data.developer.nhs.uk',
+        logonUrl: 'https://data.developer.nhs.uk/document-viewer'
     }
 };
